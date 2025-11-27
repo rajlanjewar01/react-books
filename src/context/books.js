f35f0cedc475082602f2c11e322720fdf851cec0
rajlanjewar01 <rajl.perficient@gmail.com>
2025-11-27 12:16:45 +0530

    Changing value in context

--- src/context/books.js
+++ src/context/books.js
@@ -1,5 +1,21 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const BooksContext = createContext();
 
-export default BooksContext;
+function Provider({ children }) {
+	const [count, setCount] = useState(5);
+
+	const  valueToShare = {
+		count,
+		incrementCount: () => {
+			setCount(count + 1);
+		},
+	};
+
+	return <BooksContext.Provider value={ valueToShare }>
+		{ children }
+	</BooksContext.Provider>
+}
+
+export { Provider }; // names export 
+export default BooksContext; // default export
